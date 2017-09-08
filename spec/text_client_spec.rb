09a942b4bbe15f8970e81f98587a407633f99417
Moses Mendoza <moses@puppet.com>
2017-09-08 15:55:09 -0700

    add more client spec coverage

--- spec/text_client_spec.rb
+++ spec/text_client_spec.rb
@@ -10,4 +10,23 @@ describe TextClient do
       expect(client.urls_from_text(url_string)).to eq(["http://foo.bar", "http://baz.qux"])
     end
   end
+
+  describe '#retrieve_text' do
+    it "should issue obtain the text at the provided URL" do
+      url = "http://foo.com"
+      expect(Net::HTTP).to receive(:get).with(URI(url))
+      client.retrieve_text(url)
+    end
+  end
+
+  describe "retrieve_text_from_urls" do
+    it "should retrieve text from multiple urls and aggregate the response" do
+      # We are going to return different things from #retrieve_text and expect
+      # that the aggregate of the is returned
+      expect(client).to receive(:retrieve_text).and_return("foo").exactly(4).times
+      # it doesn't matter what these urls are, what matters is how many we pass
+      urls = [:a, :b, :c, :d]
+      expect(client.retrieve_text_from_urls(urls)).to eq("foofoofoofoo")
+    end
+  end
 end
